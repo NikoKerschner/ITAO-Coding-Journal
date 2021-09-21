995f0503859e54bb07f1f803dc8ff4ca8904f691
Niko <nkerschn@nd.edu>
2021-09-21 13:18:16 -0400

    Made madlibs game

---
+++ Simple Madlib.py
@@ -0,0 +1,5 @@
+name = input("Enter a name")
+kids = int(input("Enter a number"))
+location = input("Enter a place")
+
+print(f"Hi my name is {name}, I will have {kids} kids, and I will live in {location}")
