f00eceabf70a0edc8a8224127a6da04bb74fa774
Niko <nkerschn@nd.edu>
2021-09-29 14:21:38 -0400

    fixed spacing and added slightly more

--- zoo.py
+++ zoo.py
@@ -1,4 +1,6 @@
-answer = input("Do you want to come to my zoo? yes or no").lower()
+answer = input("Do you want to come to my zoo? Please put yes or no" ).lower()
 
-if answer == "yes":
-    print("Yay")
+if answer == "no":
+    print("well screw you, please leave")
+elif answer == "yes":
+    print("nice")
