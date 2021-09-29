4d59409ff438b9c3a215feaad7674f0830341837
Niko <nkerschn@nd.edu>
2021-09-29 14:42:29 -0400

    progress

--- zoo.py
+++ zoo.py
@@ -4,11 +4,21 @@ from Animals import *
 answer = input("Do you want to come to my zoo? Please put yes or no" ).lower()
 
 if answer == "no":
-    print("well screw you, please leave")
+    print("well screw you, please leave!!!!")
 elif answer == "yes":
     print("Nice!  What animals would you like to see?")
-    firstAnimal = input("Please decide what animal you would like to see:  Your options are Fish,Shark, Dog, Bear, Lion").lower()
+    firstAnimal = input("Please decide what animal you would like to see first:  Your options are Fish,Shark, Dog, Bear, Lion ").lower()
     if firstAnimal == "shark":
-        jaws = Shark("Jaws", "10", "Grey", "Hammerhead")
+        jaws = Shark("Jaws", 10, "Grey", "Hammerhead")
         jaws.speak()
+    elif firstAnimal == "dog":
+        coop = Dog("Cooper", 2, "Golden", "Retriever")
+        coop.speak()
+    elif firstAnimal == "fish":
+        finn = Fish("Finn", 28, "Blue", "Beta fish")
+        finn.speak()
+    elif firstAnimal == "lion":
+        simba = Lion("Simba", 13, "Yellow", "Lion")
+        simba.speak()
+
 
